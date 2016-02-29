35e05acefcfa401c51147a808fcf4d8d354a213b
sarat1 <saratswety@gmail.com>
2016-02-29 17:44:47 +0530

    mail appadded

---
+++ Maill/config/environment.rb
@@ -0,0 +1,14 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'saratswety@gmail.com',
+  :password => 'mn1a04a4',
+  :domain => 'gmail.com',
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
